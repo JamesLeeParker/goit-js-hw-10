079671fd8cac8c81682694446c2dcff0a0b2b3fa
Ivan Kostenko <89205729+JamesLeeParker@users.noreply.github.com>
2021-11-21 11:54:38 +0200

    fetch func

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import './css/styles.css';
 // import fetchCountries from './fetchCountries';
+const bodyRef = document.querySelector('body');
+
+const inputRef = bodyRef.querySelector('#search-box');
+const ulRef = bodyRef.querySelector('.country-list');
+const divRef = bodyRef.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
+const BASE_URL = 'https://restcountries.com/v2/all';
 
-const BASE_URL = 'https://restcountries.com/v3.1/name';
-
-const fetchCountries = (name = '/') => {
-  return fetch(BASE_URL + name).then(countri => console.log(countri.json()));
+const fetchCountries = () => {
+  return fetch(BASE_URL)
+    .then(response => response.json())
+    .then(countries => createMarkup(countries));
 };
 
-fetchCountries('/france').then(data => console.log(data));
+inputRef.addEventListener('input', () => fetchCountries);
+
+const createMarkup = countries => {
+  const markupList = countries
+    .map(({ name, flags }) => {
+      `<li><svg>${flags.svg}</svg>${name}</li>`;
+    })
+    .join('');
+  ulRef.innerHTML = markupList;
+};
